ea34afa82934f7e26cdcb552d50756cae62561c1
Daniel Cunha <danielbrandaodacunha@gmail.com>
2018-05-28 17:28:10 -0300

    places

--- src/components/eventForm/EventForm.jsx
+++ src/components/eventForm/EventForm.jsx
@@ -9,9 +9,10 @@ import {
 	updateEvent
 
 } from '../../actions/event';
+import Script from 'react-load-script';
 
 import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
-import { combineValidators, composeValidators , isRequired, hasLengthGreaterThan} from 'revalidate';
+import { combineValidators, composeValidators, isRequired, hasLengthGreaterThan } from 'revalidate';
 
 import TextInput from '../util/TextInput';
 import TextArea from '../util/TextArea';
@@ -20,20 +21,20 @@ import DateInput from '../util/DateInput';
 import PlaceIput from '../util/PlaceInput';
 
 const category = [
-	{key: 'drinks', text: 'Drinks', value: 'drinks'},
-	{key: 'culture', text: 'Culture', value: 'culture'},
-	{key: 'films', text: 'Films', value: 'film'},
-	{key: 'food', text: 'Food', value: 'food'},
-	{key: 'music', text: 'Music', value: 'music'},
-	{key: 'travel', text: 'Travel', value: 'travel'},
+	{ key: 'drinks', text: 'Drinks', value: 'drinks' },
+	{ key: 'culture', text: 'Culture', value: 'culture' },
+	{ key: 'films', text: 'Films', value: 'film' },
+	{ key: 'food', text: 'Food', value: 'food' },
+	{ key: 'music', text: 'Music', value: 'music' },
+	{ key: 'travel', text: 'Travel', value: 'travel' },
 ];
 
 const validate = combineValidators({
-	title: isRequired({message: 'The event title is required'}),
-	category: isRequired({message: 'Please provide a category'}),
+	title: isRequired({ message: 'The event title is required' }),
+	category: isRequired({ message: 'Please provide a category' }),
 	description: composeValidators(
-		isRequired({message: 'Please enter a description'}),
-		hasLengthGreaterThan(4)({message: 'Description must have more than 5 characters'})
+		isRequired({ message: 'Please enter a description' }),
+		hasLengthGreaterThan(4)({ message: 'Description must have more than 5 characters' })
 	)(),
 	city: isRequired('city'),
 	venue: isRequired('venue'),
@@ -44,7 +45,14 @@ class EventForm extends Component {
 
 	state = {
 		cityLatLng: {},
-		
+		scriptLoaded: false
+
+	}
+
+	handleCitySelect = (selectedCity) => {
+		geocodeByAddress(selectedCity)
+			.then(results => getLatLng(results[0]))
+			.then(latlng => this.setState({ cityLatLng: latlng }));
 	}
 
 
@@ -72,39 +80,63 @@ class EventForm extends Component {
 			this.props.history.push('/events');
 		}
 	}
-	
+
+	handleScriptLoad = () => this.setState({scriptLoaded: true});
+
 	render() {
 		const { invalid, submitting, pristine } = this.props;
 		return (
-			<Grid.Column  width={10}>
+			<Grid.Column width={10}>
+				<Script
+					url="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_nTjtxOtSO3_2CnYrP3Xurrb16FoJ-RA&libraries=places"
+					onLoad={this.handleScriptLoad}
+				/>
 				<Segment>
 					<Form onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
 						<Header sub color="teal" content="Event Details" />
 						<Field name="title" type="text" component={TextInput} placeholder="Give a name to your event" />
-						<Field name="category" 
-							type="text" 
-							component={SelectInput} 
+						<Field name="category"
+							type="text"
+							component={SelectInput}
 							placeholder="What's your event about "
-							options={category} 
+							options={category}
 							multiple={false}
 
 						/>
 						<Field name="description" type="text" component={TextArea} rows={3} placeholder="Tell us about your event" />
 
 						<Header sub color="teal" content="Event Location Details" />
-						<Field name="city" type="text" component={PlaceIput} options={{types:['(cities)']}}  placeholder="Event City" />
-						<Field name="venue" type="text" component={PlaceIput} options={{types:['establishment']}} placeholder="event Venue" />
-						<Field 
-							name="date" 
-							type="text" 
-							component={DateInput} 
-							dateFormat="YYYY-MM-DD HH:mm" 
+						<Field
+							name="city"
+							type="text"
+							component={PlaceIput}
+							options={{ types: ['(cities)'] }}
+							placeholder="Event City"
+							onSelect={this.handleCitySelect}
+						/>
+						{this.state.scriptLoaded &&
+							<Field 
+								name="venue" 
+								type="text" 
+								component={PlaceIput} 
+								options={{ 
+									location: new google.maps.LatLng(this.state.cityLatLng),
+									radius: 1000,
+									types: ['address'] 
+								}} 
+								placeholder="event Venue" />
+						}
+						<Field
+							name="date"
+							type="text"
+							component={DateInput}
+							dateFormat="YYYY-MM-DD HH:mm"
 							timeFormat='HH:mm'
 							showTimeSelect
 							placeholder="Event Date" />
-						<Button 
-							disabled={ invalid || submitting || pristine}
-							positive type="submit">							
+						<Button
+							disabled={invalid || submitting || pristine}
+							positive type="submit">
 							Submit
 						</Button>
 						<Button onClick={() => this.props.history.goBack} type="button">Cancel</Button>
